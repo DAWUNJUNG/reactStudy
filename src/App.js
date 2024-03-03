1aa9d458e0ed9a911ba19c6d197a73675778a397
dawun <dawun@lacy.co.kr>
2024-03-03 17:38:03 +0900

    애플코딩 1~7강 (선행)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [titleData, setTitle] = useState({
+    'title1': '남자코트 추천',
+    'title2': '강남 우동맛집',
+    'title3': '파이썬 독학'
+  });
+  const [likeCount, setLike] = useState(0);
+  const [modalState, setModalState] = useState(false);
+
+  const firstTitleToggle = () => {
+    setTitle({
+      ...titleData,
+      'title1': (titleData.title1 === '남자코트 추천') ? '여자코트 추천' : '남자코트 추천'
+    })
+  };
+
+  return (<div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+
+      <button style={{display: "none"}} onClick={() => {
+        firstTitleToggle()
+      }}>글 수정
+      </button>
+
+      <div className="list">
+        <h4>{titleData.title1} <span onClick={() => {
+          setLike(likeCount + 1)
+        }}>👍</span> {likeCount} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{titleData.title2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4 onClick={() => {setModalState(!modalState)}}>{titleData.title3}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      {
+        modalState ? <Modal/> : null
+      }
+
     </div>
   );
 }
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+};
 
 export default App;
