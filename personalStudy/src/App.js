2314b8f8aa3e155ecbe14a8acb994c4cb9230029
dawun <dawun@lacy.co.kr>
2024-03-05 23:10:10 +0900

    애플코딩 8~9강

--- personalStudy/src/App.js
+++ personalStudy/src/App.js
@@ -3,19 +3,27 @@ import {useState} from 'react';
 
 function App() {
 
-  const [titleData, setTitle] = useState({
-    'title1': '남자코트 추천',
-    'title2': '강남 우동맛집',
-    'title3': '파이썬 독학'
-  });
-  const [likeCount, setLike] = useState(0);
-  const [modalState, setModalState] = useState(false);
+  const [contentData, setContent] = useState([
+    {
+      'title': '남자코트 추천',
+      'likeCount': 0
+    },
+    {
+      'title': '강남 우동맛집',
+      'likeCount': 0
+    },
+    {
+      'title': '파이썬 독학',
+      'likeCount': 0
+    }
+  ]);
+  const [modalState] = useState(false);
 
   const firstTitleToggle = () => {
-    setTitle({
-      ...titleData,
-      'title1': (titleData.title1 === '남자코트 추천') ? '여자코트 추천' : '남자코트 추천'
-    })
+    setContent([
+      ...contentData,
+      contentData[0].title = (contentData[0].title === '남자코트 추천') ? '여자코트 추천' : '남자코트 추천'
+    ])
   };
 
   return (
@@ -24,32 +32,26 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button style={{display: "none"}} onClick={() => {
-        firstTitleToggle()
-      }}>
-        글 수정
-      </button>
+      <button style={{display: "none"}} onClick={() => { firstTitleToggle() }}>글 수정</button>
 
-      <div className="list">
-        <h4>
-          {titleData.title1}
-          <span onClick={() => {
-            setLike(likeCount + 1)
-          }}>👍</span>
-          {likeCount}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{titleData.title2}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4 onClick={() => {
-          setModalState(!modalState)
-        }}>{titleData.title3}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {
+        contentData.map((rowData, index) => {
+          return (
+            <div className="list" key={index}>
+              <h4>
+                {rowData.title}
+                <span onClick={() => {
+                  let tmp = [...contentData];
+                  tmp[index].likeCount = rowData.likeCount + 1;
+                  setContent(tmp)
+                }}>👍</span>
+                {rowData.likeCount}
+              </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
 
       {
         modalState ? <Modal/> : null
