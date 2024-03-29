ec45d9f02f230fba6c451b80ef575ec1576410c5
dawun <dawun@lacy.co.kr>
2024-03-29 17:32:02 +0900

    기존 페이지 typescript 페이지로 컨버팅

--- personal-study-ts/pages/index.tsx
+++ personal-study-ts/pages/index.tsx
@@ -1,118 +1,169 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import * as React from 'react';
+import {useState} from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+export default function App() {
+  const [contentData, setContent] = useState([
+    {
+      'title': '남자코트 추천',
+      'content': '남자코트는 알아서 찾아보시기 바랍니다',
+      'updatedAt': '2월 17일 발행',
+      'likeCount': 0
+    },
+    {
+      'title': '강남 우동맛집',
+      'content': '맛집은 너의 생각이 다 맛집이야',
+      'updatedAt': '2월 17일 발행',
+      'likeCount': 0
+    },
+    {
+      'title': '파이썬 독학',
+      'content': '파이썬은 독학하기 좋지 쉽잖아',
+      'updatedAt': '2월 17일 발행',
+      'likeCount': 0
+    }
+  ]);
+  const [modalIndex, setModalIndex] = useState({current: null, before: null});
+  const [modalState, setModal] = useState(false);
 
-export default function Home() {
+  const createNewContent = () => {
+    const titleElement = document.getElementById('newContentTitle');
+    const contentElement = document.getElementById('newContent');
+    const today = new Date();
+
+    if (titleElement.value === '') {
+      alert('글 제목을 입력해주세요');
+      return;
+    }
+    if (contentElement.value === '') {
+      alert('글 내용을 입력해주세요');
+      return;
+    }
+
+    let tmp = [...contentData];
+    tmp.push({
+      'title': titleElement.value,
+      'content': contentElement.value,
+      'updatedAt': (today.getMonth() + 1) + '월' + (today.getDay()) + '일 발행',
+      'likeCount': 0
+    });
+    setContent(tmp);
+
+    titleElement.value = '';
+    contentElement.value = '';
+  };
+
+  const delContent = (index) => {
+    let tmp = [...contentData];
+    tmp.splice(index, 1);
+    setContent(tmp);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {
+        contentData.map((rowData, index) => {
+          return (
+            <div className="list" key={index}>
+              <h4>
+                <span onClick={() => {
+                  if (modalIndex?.current === index && modalState) {
+                    setModal(false);
+                  } else if (!modalState) {
+                    setModal(true);
+                  }
+
+                  setModalIndex({
+                    'before': modalIndex?.current,
+                    'current': index
+                  });
+                }}>
+                    {rowData.title}
+                  <span onClick={(e) => {
+                    e.stopPropagation();
+
+                    let tmp = [...contentData];
+                    tmp[index].likeCount = rowData.likeCount + 1;
+                    setContent(tmp);
+                  }}>👍</span>
+                  {rowData.likeCount}
+                </span>
+              </h4>
+              <p>{rowData.updatedAt}</p>
+              <span>
+                  <button onClick={() => { delContent(index) }}>삭제</button>
+              </span>
+            </div>
+          )
+        })
+      }
+
+      <div>
+        <div>글 제목 : <input type="text" id="newContentTitle"/></div>
+        <div>글 내용 : <input type="text" id="newContent"/></div>
+        <button onClick={() => { createNewContent() }}>글 생성</button>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {
+        modalState ? <Modal data={contentData} index={modalIndex} modifyData={setContent}/> : null
+      }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+const Modal = (props) => {
+  return (
+    <div className="modal">
+      <div id="viewContent">
+        <h4>{props.data[props.index.current].title}</h4>
+        <p>{props.data[props.index.current].content}</p>
+        <p>{props.data[props.index.current].updatedAt}</p>
+        <button  id="modifyBtn" onClick={() => {
+          document.getElementById('viewContent').classList.add('hide');
+          document.getElementById('inputContent').classList.remove('hide');
+        }}>글수정</button>
+      </div>
+      <div id="inputContent" className="hide">
+        <div>
+          글 제목 : <input type="text" id="titleInput" defaultValue={props.data[props.index.current].title}/>
+        </div>
+        <div>
+          글 내용 : <input type="text" id="contentInput" defaultValue={props.data[props.index.current].content}/>
+        </div>
+        <button id="submitBtn" onClick={() => {
+          let tmp = [...props.data];
+          const today = new Date();
+
+          const title = document.getElementById('titleInput').value;
+          const content = document.getElementById('contentInput').value;
+
+          if (title === '') {
+            alert('글 제목을 입력해주세요');
+            return;
+          }
+          if (content === '') {
+            alert('글 내용을 입력해주세요');
+            return;
+          }
+
+          tmp[props.index.current] = {
+            'title': title,
+            'content': content,
+            'updatedAt': (today.getMonth() + 1) + '월' + (today.getDay()) + '일 발행',
+            'likeCount': props.data[props.index.current].likeCount
+          };
+          props.modifyData(tmp);
+
+          alert('수정 되었습니다.');
+
+          document.getElementById('viewContent').classList.remove('hide');
+          document.getElementById('inputContent').classList.add('hide');
+        }}>적용</button>
+      </div>
+    </div>
+  )
+};
